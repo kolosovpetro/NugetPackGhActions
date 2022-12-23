c570711c0bf1ab62a85cba4ed814a93ac49762ca
kolosovpetro <kolosovp94@gmail.com>
2022-12-23 20:56:33 +0100

    feed consumer

---
+++ PackageConsumer/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MyNugetPackage;
+
+Console.WriteLine("Hello, World!");
+
+var result = MySampleService.MethodOne();
+
+Console.WriteLine(result);
